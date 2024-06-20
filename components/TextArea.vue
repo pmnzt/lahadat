fb6d67e02a60dabcfd5a8857e0b811604a4ed578
pmnzt <vponsve@gmail.com>
2024-06-20 19:30:29 +0300

    haha fix the hydration mismatch errors

--- components/TextArea.vue
+++ components/TextArea.vue
@@ -17,8 +17,7 @@ interface fieldProps {
 
             <div relative>
                 <textarea min-h="10em" max-h="20em" rows="5" v-model="model" dir="auto" py="0.3em" px="0.3em" focus="ring-2 ring-[#FF9F92] outline-none"
-                    bg="[#E8E7EA]" text="black" w="full" border="1 rounded-md [#E0E0E0]" v-bind="$attrs">
-                </textarea>
+                    bg="[#E8E7EA]" text="black" w="full" border="1 rounded-md [#E0E0E0]" v-bind="$attrs"></textarea>
 
                 <div v-if="!model" text="[#888]" pointer-events-none absolute inset-0 flex justify-between p-2>
                     <slot name="placeholder" />
