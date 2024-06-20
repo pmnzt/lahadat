52dacdec08b9ed44e73a6a75948f67735a56e098
pmnzt <vponsve@gmail.com>
2024-06-20 15:09:57 +0300

    pattern

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div h-full relative>
-    <div overflow-clip h-full absolute inset-0 pointer-events-none>
+    <div overflow-clip h-full absolute inset-0 pointer-events-none w-full>
       <Effect />
+
+      <Effect2 />
+
+
     </div>
 
     <div z="10">
